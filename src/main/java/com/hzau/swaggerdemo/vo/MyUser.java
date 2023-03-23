b7a82f314d358db6b50a761dcbe26a6e9c55b97f
yueyiming <yueyiming0513@qq.com>
2023-03-23 17:44:40 +0800

    update project name

--- src/main/java/com/hzau/swaggerdemo/vo/MyUser.java
+++ src/main/java/com/hzau/swaggerdemo/vo/MyUser.java
@@ -1,10 +1,8 @@
 package com.hzau.swaggerdemo.vo;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -28,7 +26,6 @@ public class MyUser {
     @ApiModelProperty(value = "性别",example="男")
     private String sex;
     @ApiModelProperty(value = "创建日期",dataType = "java.lang.String",example="2022-07-10 10:23:52")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//    @JsonFormat(pattern ="yyyy-MM-dd/HH:mm:ss")
     private LocalDateTime createTime;
 }
--- src/main/java/com/hzau/swaggerdemo/vo/MyUserImg.java
+++ src/main/java/com/hzau/swaggerdemo/vo/MyUserImg.java
@@ -1,10 +1,8 @@
 package com.hzau.swaggerdemo.vo;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
@@ -30,8 +28,7 @@ public class MyUserImg {
     @ApiModelProperty(value = "性别",example="男")
     private String sex;
     @ApiModelProperty(value = "创建日期",dataType = "java.lang.String",example="2022-07-10 10:23:52")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern ="yyyy-MM-dd HH:mm:ss")
+//    @JsonFormat(pattern ="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private LocalDateTime createTime;
 
     private MultipartFile file;
